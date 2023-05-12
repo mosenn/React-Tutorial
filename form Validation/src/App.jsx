520e029b317dc5377ac8b5035445f320b41e68e7
username <mohseen_abasi@yahoo.com>
2023-05-12 10:16:56 +0330

    validation form

--- form Validation/src/App.jsx
+++ form Validation/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 
 import "./App.css";
+import FormValidation from "./components/FormValidation";
 
 function App() {
   return (
     <div>
       <h1>hi react</h1>
+      <FormValidation />
     </div>
   );
 }
